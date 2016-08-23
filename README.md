70b11204572197e4bfc159acb16c11a6619efe02
zhengwei.xu <xuzhengwei.amor@gmail.com>
2016-08-23 14:11:21 +0800

    Add some basic options to zephyr editor

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# zephyr-editor -- A lightweight rich text editor .
+# What is Zephyr-Editor ?
+  Zephyr-Editor is a  lightweight rich text editor . Based on jQuery.
+# How to use ?
+First of all , you need to create a dom element called textarea.
+``` html
+<textarea id="text"></textarea>
+```
+Then , add the zephyr.js to your HTML script tag . Note that , you need to import jQuery first.
+``` javascript
+<script src="../external/jquery/jquery.js"></script>
+<script src="../src/main.js"></script>
+<script>
+    $(document).ready(function(){
+        $("#text").zephyr({"cols":"200","rows":"20"});
+    });
+</script>
+```
--- src/main.js
+++ src/main.js
@@ -1,6 +1,29 @@
-(function (window, undefined) {
-    var zephyr = {
-        hello: "hello"
-    };
-    window.zephyr = zephyr;
-})(window);
+//加一个分号，防止别人的代码没有以
+//分号结束时以下代码无法运行
+; (function (window, $, undefined) {
+
+    //构造函数
+    var Zephyr = function (domEle, options) {
+        this.$element = domEle;
+        this.defaults = {
+            "cols": "100",
+            "rows": "10"
+        }
+        this.options = $.extend({}, this.defaults, options);
+    }
+
+    //私有公有函数
+    Zephyr.prototype = {
+        create: function () {
+            return this.$element.attr({
+                "cols": this.options.cols,
+                "rows": this.options.rows
+            });
+        }
+    }
+
+    $.fn.zephyr = function (options) {
+        var editor = new Zephyr(this, options);
+        return editor.create();
+    }
+})(window, jQuery);
